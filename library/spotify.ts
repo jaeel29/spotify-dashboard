3b37f0406acfb60125ab816e867662a6cb9724c4
Jaber Elferkh <elferkhjaber@gmail.com>
2022-05-18 01:33:17 +0100

    feat: Finish NextAuth Spotify && Link with Spotify API

--- library/spotify.ts
+++ library/spotify.ts
@@ -1,34 +1,33 @@
 import SpotifyWebApi from 'spotify-web-api-node';
 
-const scopes = [
-  'user-read-email',
-  'playlist-read-private',
-  'playlist-read-collaborative',
-  'streaming',
-  'user-read-private',
-  'user-library-read',
-  'user-top-read',
-  // 'user-library-modify',
-  'user-read-playback-state',
-  'user-modify-playback-state',
-  'user-read-currently-playing',
-  'user-read-recently-played',
-  'user-follow-read',
-].join(',');
-
 const params = {
-  scope: scopes,
+  scope: [
+    'user-read-email',
+    'playlist-read-private',
+    'playlist-read-collaborative',
+    'streaming',
+    'user-read-private',
+    'user-library-read',
+    'user-top-read',
+    // 'user-library-modify',
+    'user-read-playback-state',
+    'user-modify-playback-state',
+    'user-read-currently-playing',
+    'user-read-recently-played',
+    'user-follow-read',
+  ].join(','),
 };
 
-//^ https://accounts.spotify.com/authorize?params=...
+console.log(params.scope);
 
 const queryParamsString = new URLSearchParams(params);
 
 export const LOGIN_URL = `https://accounts.spotify.com/authorize?${queryParamsString.toString()}`;
+// export const LOGIN_URL = 'https://accounts.spotify.com/authorize?scope=user-read-email';
 
 const spotifyApi = new SpotifyWebApi({
-  clientId: process.env.NEXT_PUBLIC_CLIENT_ID!,
-  clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET!,
+  clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+  clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
 });
 
 export default spotifyApi;
