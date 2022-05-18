f3b0fab88c1c5f95b393daa84972335b1d3e4bfd
Jaber Elferkh <elferkhjaber@gmail.com>
2022-05-18 01:36:09 +0100

    feat: Add Login page

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -13,8 +13,8 @@ async function refreshAccessToken(token: any) {
     return {
       ...token,
       accessToken: refreshedToken.access_token,
-      accessTokenExpires: Date.now() + refreshedToken.expires_in * 1000,
       refreshToken: refreshedToken.refresh_token ?? token.refreshToken, // Fall back to old refresh token
+      accessTokenExpires: Date.now() + refreshedToken.expires_in * 1000,
     };
   } catch (error) {
     console.log(error);
@@ -34,13 +34,14 @@ export default NextAuth({
       clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET!,
       authorization: LOGIN_URL,
     }),
-
-    // ...add more providers here
   ],
+
   secret: process.env.JWT_SECRET,
+
   pages: {
     signIn: '/login',
   },
+
   callbacks: {
     async jwt({ token, account, user }: any) {
       // initial signin
@@ -50,7 +51,7 @@ export default NextAuth({
           accessToken: account.access_token,
           refreshToken: account.refresh_token,
           username: account.providerAccountId,
-          accessTokenExpires: account.expires_at! * 1000,
+          accessTokenExpires: account.expires_at * 1000,
         };
       }
 
@@ -64,6 +65,7 @@ export default NextAuth({
       console.log('Access Token has EXPIRED, Refreshing ♻️');
       return await refreshAccessToken(token);
     },
+
     async session({ session, token }: any) {
       session.user.accessToken = token.accessToken;
       session.user.refreshToken = token.refreshToken;
